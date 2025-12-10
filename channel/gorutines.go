2ffeff7c2584f3a2636bc787352045ca288dc460
microvave <suneaoe7474@gmail.com>
2025-12-11 01:39:16 +0700

    gorutines

---
+++ channel/gorutines.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(from, ":", i)
+
+	}
+}
+
+func main() {
+
+	go f("Hello Microwave")
+	go f("hello microave")
+	time.Sleep(5 * time.Second)
+
+}
